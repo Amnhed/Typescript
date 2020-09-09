ceff74470c13c698766bfd0cc1e4386d4905520e
Amnhed <aalhe0.0@gmail.com>
2020-09-09 18:37:53 -0500

    Creando archivo type script
    Transpilar archivo app.ts typescript para crear app.js
    >> tsc app.ts

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-
-
-function saludar( nombre ) {
-    console.table( 'Hola ' + nombre ); // Hola Logan
-}
-
-
-const wolverine = {
-    nombre: 'Logan'
-};
-
-
-saludar(  );
-
+(function () {
+    //funcion anonima autoinvicada
+    function saludar(nombre) {
+        console.table('Hola ' + nombre); // Hola Logan
+    }
+    var wolverine = {
+        nombre: 'Logan'
+    };
+    saludar(wolverine.nombre);
+})();
