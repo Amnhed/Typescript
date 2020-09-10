2ae78ca85aa565058be2780ebd707431ec5f9437
Amnhed <aalhe0.0@gmail.com>
2020-09-10 13:13:57 -0500

    Templates literales del ems6
    Mostrar estrin con variable
    ` ${ variable } `

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 "use strict";
 (function () {
-    var mensaje = 'Hola';
-    var numero = 12345;
-    var booleano = true;
-    var hoy = new Date();
-    var mensajeonumero;
-    mensajeonumero: mensaje;
-    mensajeonumero: numero;
-    //spiderman = {} Is missin propierities nombre and edad
-    //Siempre debo declarar objeto con las mismas propiedades por que typescript lo valida
-    var spiderman = {
-        nombre: 'Peter',
-        edad: 30
+    var getedad = function () {
+        return 100 - 70;
     };
-    spiderman = {
-        nombre: 'Tony',
-        edad: 35
-    };
-    console.log(spiderman);
+    var nombre = 'Amnhed';
+    var apelllido = 'Lagunas';
+    var salida = nombre + " " + apelllido + " ( " + getedad() + ")";
+    console.log(salida);
 })();
