f653a34d3182da796f55c8e221d5ad249548c7c1
Amnhed <aalhe0.0@gmail.com>
2020-09-10 12:34:38 -0500

    Excluir archivo de transpilado de typescript
    en el archivo tsconfig agregar exclude

---
+++ Typescript/app.ts
@@ -0,0 +1,33 @@
+(function(){
+     let mensaje: string = 'Hola';
+     let numero: number = 12345;
+     let booleano: boolean = true;
+     let hoy: Date = new Date();
+
+     let mensajeonumero: string | number;
+     mensajeonumero: mensaje;
+     mensajeonumero: numero;
+
+     //spiderman = {} Is missin propierities nombre and edad
+     //Siempre debo declarar objeto con las mismas propiedades por que typescript lo valida
+
+     let spiderman = {
+         nombre: 'Peter',
+         edad: 30
+     }
+
+     spiderman = {
+         nombre: 'Tony',
+         edad: 35
+     }
+     console.log(spiderman);
+
+
+
+})();
+
+
+
+
+
+
