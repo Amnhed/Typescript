66d177a243f80aaf511e4bc7ecd94e02d8881d70
Amnhed <aalhe0.0@gmail.com>
2020-09-10 14:43:40 -0500

    Funciones: parametro obligatorios , opcionales y por defecto
    Parametro opcional

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 "use strict";
 (function () {
-    var getedad = function () {
-        return 100 - 70;
-    };
-    var nombre = 'Amnhed';
-    var apelllido = 'Lagunas';
-    var salida = nombre + " " + apelllido + " ( " + getedad() + ")";
-    console.log(salida);
+    // funciones y parametros opcionales
+    // se deben ordenar
+    //1 parametros obligatorios
+    //2 opcionales con ?:
+    //3 valor por defecto
+    function activar(quien, momento, objeto) {
+        if (objeto === void 0) { objeto = 'batiseñal'; }
+        if (momento) {
+            console.log(quien + " activo la " + objeto + " en la " + momento);
+        }
+        else {
+            console.log(quien + " activo la " + objeto);
+        }
+    }
+    activar('gordon');
 })();
--- app.ts
+++ app.ts
@@ -1,13 +1,21 @@
 (function(){
-    const getedad = () =>{
-        return 100-70
-    }
-    const nombre = 'Amnhed'
-    const apelllido = 'Lagunas'
+    // funciones y parametros opcionales
+    // se deben ordenar
+    //1 parametros obligatorios
+    //2 opcionales con ?:
+    //3 valor por defecto
+    function activar ( quien : string,
+        momento?: string,
+        objeto: string = 'batiseñal'
+        ){
+            if(momento){
+                console.log(`${ quien } activo la ${ objeto } en la ${ momento }`)
+            }else{
+                console.log(`${ quien } activo la ${ objeto }`)
+            }
 
-    let salida = `${ nombre } ${ apelllido } ( ${getedad()})`
-     
-     console.log(salida);
+    }
+    activar('gordon')
 
 
 
