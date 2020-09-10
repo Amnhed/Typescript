5701404d17e7ddd3bc4f2284c3cab50750b6e77a
Amnhed <aalhe0.0@gmail.com>
2020-09-10 14:14:59 -0500

    Renombre archivos correctamente

---
+++ Typescript/Templates literales esc6.ts
@@ -0,0 +1,20 @@
+(function(){
+    const getedad = () =>{
+        return 100-70
+    }
+    const nombre = 'Amnhed'
+    const apelllido = 'Lagunas'
+
+    let salida = `${ nombre } ${ apelllido } ( ${getedad()})`
+     
+     console.log(salida);
+
+
+
+})();
+
+
+
+
+
+
